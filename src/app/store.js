c6f15e192ad52c390192df0d24ac2d8dd8c2866d
James Kennedy <acinea.kennedy@gmail.com>
2023-02-06 15:55:17 -0500

    Setup SearchBar Slice / Reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {default as SearchBarSliceReducer} from '../features/SearchBar/searchbarSlice';
 
 export const store = configureStore({
   reducer: {
-    },
+    searchbar: SearchBarSliceReducer,
+    }
 });
--- src/features/SearchBar/searchbar.js
+++ src/features/SearchBar/searchbar.js
@@ -1,8 +1,24 @@
 import React, {useState} from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './searchbar.css'
 
 export function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const handleKeyPress = (event) => {
+        if(event.keyCode === 13) {
+            event.preventDefault();
+            dispatch(loadSearchResults(searchTerm))
+            .then(()=> navigate(`/search/${searchTerm}`))
+            .then(()=> setSearchTerm(''));
+        }
+    }
 
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
 
     return (
         <form className="search-bar">
@@ -10,9 +26,9 @@ export function SearchBar() {
                 className="search-input"
                 type="search"
                 placeholder="Search..."
-                //value={searchTerm}
-                //onChange={handleChange}
-                //onKeyDown={handleKeyPress}
+                value={searchTerm}
+                onChange={handleChange}
+                onKeyDown={handleKeyPress}
             />
         </form>
     )
