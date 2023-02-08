5c9088ee9a77db08c71ee072525f58f1abaea089
James Kennedy <acinea.kennedy@gmail.com>
2023-02-07 20:41:47 -0500

    Setup Router for nav buttons, will attempt to rework rendering

--- src/features/SearchBar/searchbar.js
+++ src/features/SearchBar/searchbar.js
@@ -1,6 +1,10 @@
 import React, {useState} from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Article } from '../Articles/article';
 import { Best } from '../Homepage/Best/best';
+import { Hot } from '../Homepage/Hot/hot'
+import { New } from '../Homepage/New/new'
+import { Top } from '../Homepage/Top/top'
 import './searchbar.css'
 
 export function SearchBar() {
@@ -8,18 +12,28 @@ export function SearchBar() {
     const handleChange = (e) => {
         setSearchTerm(e.target.value)
     }
-
+            
     return (
-        <form className="search-bar" onSubmit={e => {e.preventDefault(); }}>
-            <input 
-                className="search-input"
-                type="search"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={handleChange}
-            />
-            {<Article searchTerm={searchTerm} />}
-            {<Best searchTerm={searchTerm} />}
-        </form>
+        <div>
+            <form className="search-bar" onSubmit={e => {e.preventDefault(); }}>
+                <input 
+                    className="search-input"
+                    type="search"
+                    placeholder="Search..."
+                    value={searchTerm}
+                    onChange={handleChange}
+                />
+            </form>
+            <div className='post-container'>
+                <Routes>
+                    <Route path='/' element={<Article searchTerm={searchTerm}/>} />
+                    <Route path='/best' element={<Best searchTerm={searchTerm}/>} />
+                    <Route path='/hot' element={<Hot searchTerm={searchTerm}/>} />
+                    <Route path='/new' element={<New searchTerm={searchTerm}/>} />
+                    <Route path='/top' element={<Top searchTerm={searchTerm}/>} />``
+                </Routes>
+            </div>
+        </div>
+
     )
 }
