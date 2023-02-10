f353c69c7ebfb41a6ace4aee30553a47186ebe22
James Kennedy <acinea.kennedy@gmail.com>
2023-02-09 20:16:12 -0500

    Configured Post to determine if the reddit post has a video or image, and to render accordingly

--- src/features/Post/post.js
+++ src/features/Post/post.js
@@ -6,17 +6,17 @@ export function Post(props) {
     return(
         <article className='post-container'>
             <div className='post-title'>
-                {/*<h6>Posted by {props.article.author}</h6>*/}
-                {/*<a href={'https://www.reddit.com' + props.article.permalink} target="_blank" rel="noreferrer">
+                <h6>Posted by {props.article.author}</h6>
+                <a href={'https://www.reddit.com' + props.article.permalink} target="_blank" >
                     <h3>{props.article.title}</h3>
-                </a>*/}
-                <h6>Posted by Author</h6>
-                <h3>Title</h3>
+                </a>
+                
             </div>
             <div className='post-contents'>
-                {/*<p>{props.article.selftext}</p>*/}
-                <p>Text</p>
-                {/*<img src={props.article.thumbnail}  />*/}
+                <p>{props.article.selftext}</p>
+                {props.article.is_video === true ? <video src={props.article.media.reddit_video.fallback_url} type="video/mp4" controls /> 
+                : props.article.preview ? <img src={props.article.url} /> 
+                : '' }
             </div>
         </article>
         )
