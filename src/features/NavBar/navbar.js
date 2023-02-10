147a47157af2680f89e44ee0bc484df5c74be175
James Kennedy <acinea.kennedy@gmail.com>
2023-02-09 21:50:52 -0500

    Updated active-nav color change using NavLink. Removed obsolete functions.

--- src/features/NavBar/navbar.js
+++ src/features/NavBar/navbar.js
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 import RocketIcon from '@mui/icons-material/Rocket'
 import WhatshotIcon from '@mui/icons-material/Whatshot'
 import NewReleasesIcon from '@mui/icons-material/NewReleases'
@@ -7,40 +6,32 @@ import LeaderboardIcon from '@mui/icons-material/Leaderboard'
 import './navbar.css'
 
 export function NavBar() {
-    const [activeButton, setActiveButton] = useState('');
-
-    const handleClick = (e) => {
-        const name  = e.target.id
-        console.log(name)
-        setActiveButton(name)
-        console.log(activeButton)
-    }
 
     return (
         <nav className='nav-container'>
             <ul className='nav-list'>
                 <li className='nav-list-item' >
-                    <NavLink to='/best' className={activeButton === "1" ? "active-button" : "nav-list-link"}  >
-                        <RocketIcon  id="1" className='nav-list-icon' fontSize='large' state="best" onClick={handleClick}/>
-                        <span id="1" className='nav-title' onClick={handleClick}>Best</span>
+                    <NavLink to='/best' className={ ({isActive}) => isActive ? "active-button" : "nav-list-link"}  >
+                        <RocketIcon className='nav-list-icon' fontSize='large' />
+                        <span className='nav-title'>Best</span>
                     </NavLink>
                 </li>
                 <li className='nav-list-item' >
-                    <NavLink to='/hot' className={activeButton === "2" ? "active-button" : "nav-list-link"} >
-                        <WhatshotIcon id="2" className='nav-list-icon' fontSize='large' onClick={handleClick}/>
-                        <span id="2" className='nav-title' onClick={handleClick}>Hot</span>
+                    <NavLink to='/hot' className={ ({isActive}) => isActive ? "active-button" : "nav-list-link"} >
+                        <WhatshotIcon className='nav-list-icon' fontSize='large' />
+                        <span className='nav-title' >Hot</span>
                     </NavLink>
                 </li>
                 <li className='nav-list-item'  >
-                    <NavLink to='/new' className={activeButton === "3" ? "active-button" : "nav-list-link"} onClick={handleClick}>
+                    <NavLink to='/new' className={ ({isActive}) => isActive ? "active-button" : "nav-list-link"} >
                         <NewReleasesIcon id="3" className='nav-list-icon' fontSize='large' />
-                        <span id="3" className='nav-title' onClick={handleClick}>New</span>
+                        <span className='nav-title'>New</span>
                     </NavLink>
                 </li>
                 <li className='nav-list-item'>
-                    <NavLink to='/top' className={activeButton === "4" ? "active-button" : "nav-list-link"} >
-                        <LeaderboardIcon id="4" className='nav-list-icon' fontSize='large' onClick={handleClick}/> 
-                        <span id="4" className='nav-title' onClick={handleClick}>Top</span>
+                    <NavLink to='/top' className={ ({isActive}) => isActive ? "active-button" : "nav-list-link"} >
+                        <LeaderboardIcon className='nav-list-icon' fontSize='large' /> 
+                        <span className='nav-title'>Top</span>
                     </NavLink>
                 </li>
             </ul>
