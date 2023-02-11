cddc47db4f095df8e98b436be0e30da162ee1e15
James Kennedy <acinea.kennedy@gmail.com>
2023-02-11 12:18:49 -0500

    Created comments component to show comments for posts

---
+++ src/features/Comments/comments.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import './comments.css'
+
+export const Comments = ({comments}) => {
+    const [clicked, setClicked] = useState('0')
+    const upVoted = () => {
+        clicked === '1' ? setClicked('0') : setClicked('1')
+    };
+    const downVoted = () => {
+        clicked === '-1' ? setClicked('0') : setClicked('-1')
+    };
+    return(
+        <div>
+            <ul className="individual-comment-container">
+                <div className="comment-score">
+                    <ArrowUpwardIcon className={clicked === '1' ? 'upvoted' : 'upvote'} onClick={upVoted} />
+                    {comments.score}
+                    <ArrowDownwardIcon className={clicked === '-1' ? 'downvoted' : 'downvote'}onClick={downVoted} />
+                </div>
+                <div className="comment-content">
+                    <h5>{comments.author}</h5>
+                    <p>{comments.body}</p>
+                </div>
+            </ul>
+        </div>
+    )
+}
+
+export default Comments;
