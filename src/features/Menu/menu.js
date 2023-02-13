3459f7848d9d31a15971591aab7fbdc2a1a87353
James Kennedy <acinea.kennedy@gmail.com>
2023-02-12 22:09:52 -0500

    Reworked the pop-out menu. Added Top Communities list

--- src/features/Menu/menu.js
+++ src/features/Menu/menu.js
@@ -1,43 +1,44 @@
-import React, { useState } from 'react'
-import Menu from '@mui/material/Menu'
+import React, { useState, useEffect } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
 import './menu.css'
 
 export function HeaderMenu() {
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
+    const [communities, setCommunities] = useState([]);
+    const [open, setOpen] = useState('0');
+
+    const toggleMenu = () => {
+        {open === '1' ? setOpen('0') : setOpen('1')}
+        console.log('clicked!')
     }
 
+    useEffect(() => {
+        fetch("https://www.reddit.com/reddits" + ".json").then(res => {
+            if (res.status !== 200) {
+                console.log('Error');
+                return;
+            }
+            res.json().then(data => {
+                if (data !== null) {
+                    setCommunities(data.data.children.map(({data}) => data))
+                }
+            });
+        })
+    }, [])
+
+
     return (
-        <div>
+        <div className='menu-container'>
             <MenuIcon 
             className='header-menu' 
             fontSize="large"
             style={{color: '#484D45'}}    
-        
-            onClick={handleClick} />
-            <Menu
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            PaperProps={{
-                sx: {
-                    width: '20ch',
-                }
-            }}
+            onClick={toggleMenu}
+            />
 
-            >
-                <div className="open-menu">
-                <h3>Testing Menu</h3>
-                <h3>Top</h3>
-                <h3>Communities</h3>
-                </div>
-            </Menu>
+            <div className={open === '0' ? "close-menu" : "open-menu"}>
+                <h3 className="menu-title">Top Communities</h3>
+                {communities.map(community => <ul>{community.title}</ul>)}
+            </div>
 
         </div>
 )}
