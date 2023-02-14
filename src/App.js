3114b8f9bb99f070c07ef883fe307c01c25bc036
James Kennedy <acinea.kennedy@gmail.com>
2023-02-13 21:28:21 -0500

    Removed unnessecary imports.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './features/Header/header'
 import { NavBar } from './features/NavBar/navbar';
-import { Menu } from './features/Menu/menu'
 import { SearchBar } from './features/SearchBar/searchbar';
-import { Post } from './features/Post/post'
 
 
 
