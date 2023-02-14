1dc636e30cb1c5ce86d8f0f930fcac111f92a671
James Kennedy <acinea.kennedy@gmail.com>
2023-02-13 21:27:43 -0500

    Reworked Header Menu for compatibility with the SearchBar Component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './features/Header/header'
 import { NavBar } from './features/NavBar/navbar';
-import { HeaderMenu } from './features/Menu/menu'
+import { Menu } from './features/Menu/menu'
 import { SearchBar } from './features/SearchBar/searchbar';
 import { Post } from './features/Post/post'
 
@@ -12,7 +12,6 @@ function App() {
   return (
     <Router>
         <Header />
-        <HeaderMenu />
         <NavBar />
         <SearchBar />
      </Router> 
