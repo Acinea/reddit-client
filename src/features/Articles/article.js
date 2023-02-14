08ce1d389e80ea91feaf4801ecc7a0c64329a6f8
James Kennedy <acinea.kennedy@gmail.com>
2023-02-13 23:06:28 -0500

    Updated location to work with github pages.

--- src/features/Articles/article.js
+++ src/features/Articles/article.js
@@ -6,9 +6,10 @@ import { useLocation } from 'react-router-dom';
 export function Article(props) {
     const [articles, setArticles] = useState([]);
     const location = useLocation();
+    const filteredLocation = location.pathname.replace('/reddit-client', '')
 
     useEffect(() => {
-        fetch("https://www.reddit.com/r/" + props.searchTerm + location.pathname + ".json").then(res => {
+        fetch("https://www.reddit.com/r/" + props.searchTerm + filteredLocation + ".json").then(res => {
             if (res.status !== 200) {
                 console.log('Error');
                 return;
